c08e90b484a4382793c7c07ddcd2bfde057c086c
oscarherra <oscar.herra@ucr.ac.cr>
2026-06-06 22:46:50 -0600

    cambios

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
@@ -18,9 +18,7 @@ api.interceptors.request.use(
 
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default api;
